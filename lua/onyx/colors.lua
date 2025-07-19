4d3aaf06fab1da530be1509eea772d785ad9382b
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2025-07-19 05:26:19 +0200

    Update colors

--- lua/onyx/colors.lua
+++ lua/onyx/colors.lua
@@ -1,19 +1,21 @@
 return {
     none = "NONE",
 
-    deep = "#1a1a1a",
-    base = "#202020",
+    deep = "#1e1e1e",
+    base = "#222222",
 
-    smol = "#404040",
-    dim = "#505050",  -- comments
-    meh = "#909090",
-    text = "#cccccc",
+    smol = "#383838",
+    dim = "#525252",
+    meh = "#929292",
+    text = "#c8c8c8",
 
     -- red = "#ddaec3",
     -- red = "#f89dc9",  -- higher chroma
-    red = "#ff78be",  -- even higher chroma
+    -- red = "#ff78be",  -- even higher chroma
+    red = "#fd7eaa",  -- custom
+
     peach = "#deb39c",
-    -- mire = "#c0c295",
+    mire = "#c0c295",
     green = "#97cbb8",
     blue = "#96c6dd",
     purp = "#bcb8e2",
@@ -22,16 +24,7 @@ return {
     -- l = 0.8, c = 0.06, h = 50 + 60 * n
     -- https://oklch.com
 
-    -- old colors:
-    -- blue = "#99c0d4",
-    -- func = "#99b898",
-    -- purp = "#bab0e4",
-    -- red = "#fd70a6",
-    -- green = "#93e8c7",
-    -- yellow = "#f9e2af",
-    -- peach = "#fab489",
-
-    -- old colors in oklch:
+    -- old colors (oklch):
     -- text 0.85
     -- gree 0.87 0.10 168
     -- blue 0.79 0.05 230
