1db5a5ebd4280cf8f3053249dce3ff5fcf01932e
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-01-07 00:54:06 +0100

    Update colors

--- lua/onyx/colors.lua
+++ lua/onyx/colors.lua
@@ -1,18 +1,15 @@
 return {
     none = "NONE",
 
-    deep = "#1c1c1c",
+    deep = "#1a1a1a",
     base = "#202020",
 
-    smol = "#383838",
+    smol = "#363636",
     dim = "#525252",
-    meh = "#888888",
-    uhm = "#b0b0b0",
+    meh = "#8b8b8b",
     text = "#c8c8c8",
 
-    -- red = "#ddaec3",
     red = "#fd7eaa",  -- custom
-
     peach = "#deb39c",
     mire = "#c0c295",
     green = "#97cbb8",
@@ -20,7 +17,7 @@ return {
     purp = "#bcb8e2",
     aqua = "#b2c4d0",
 
-    -- new colors generated using oklch:
+    -- colors generated using oklch:
     -- l = 0.8, c = 0.06, h = 50 + 60 * n
     -- https://oklch.com
 }
