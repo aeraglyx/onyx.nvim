e6b2565d4c9404b13dec20ec2cc0d2ce7918ac60
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-09-23 04:10:25 +0200

    Update colors

--- lua/onyx/colors.lua
+++ lua/onyx/colors.lua
@@ -9,15 +9,14 @@ return {
     meh = "#8b8b8b",
     text = "#c8c8c8",
 
-    red = "#fd7eaa",  -- custom
-    peach = "#deb39c",
-    mire = "#c0c295",
-    green = "#97cbb8",
-    blue = "#96c6dd",
-    purp = "#bcb8e2",
-    aqua = "#b2c4d0",
+    red   = "#fb9dc2",
+    peach = "#d3b8a6",
+    green = "#bcc1a4",
+    teal  = "#a4c7bc",
+    blue  = "#a6c3d4",
+    purp  = "#bfb9d5",
 
     -- colors generated using oklch:
-    -- l = 0.8, c = 0.06, h = 50 + 60 * n
+    -- l = 0.8, c = 0.04, h = 55 + 60 * n
     -- https://oklch.com
 }
